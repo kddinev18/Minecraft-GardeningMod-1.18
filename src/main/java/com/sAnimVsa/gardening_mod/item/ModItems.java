25b10c108a34deaf27401d2ff85d446723b3646e
kddinev18 <kddinev18@codingburgas.bg>
2022-03-13 12:07:56 +0200

    Add new item called "poop"

---
+++ src/main/java/com/sAnimVsa/gardening_mod/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.sAnimVsa.gardening_mod.item;
+
+import com.sAnimVsa.gardening_mod.GardeningMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems
+{
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GardeningMod.MOD_ID);
+
+    public static final RegistryObject<Item> POOP = ITEMS.register("poop",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public  static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
